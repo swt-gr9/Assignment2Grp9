cea7e8e8bc9c269686e068fd0b4dd9d2ad52a05f
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-03-06 11:58:52 +0100

    Nyt design

--- AirTrafficMonitor/Calculator/SHCalculator.cs
+++ AirTrafficMonitor/Calculator/SHCalculator.cs
@@ -2,7 +2,21 @@
 
 namespace Calculator
 {
-    public class SHCalculator
+    public static class SHCalculator
     {
+        public static double GetCurrentSpeed(int x, int y, double time)
+        {
+            return (double)GetDistance(x, y) / time;
+        }
+
+        private static double GetDistance(int x, int y)
+        {
+            return Math.Sqrt((Math.Pow(x, 2) + Math.Pow(y, 2)));
+        }
+
+        public static int GetCurrentHeading(int x, int y)
+        {
+            
+        }
     }
 }
