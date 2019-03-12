a9378fb5611265716e102b107d0ee13f7cfc3070
Batmancarlo <43032221+Batmancarlo@users.noreply.github.com>
2019-03-12 12:50:43 +0100

    DataFormatter Addet

---
+++ AirTrafficMonitor/AirTrafficMonitoringSystem/DataFormatter/DataFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoringSystem.DataFormatter
+{
+    class DataFormatter
+    {
+
+        public DataFormatter() { }
+
+        Plane.Plane FormatFromString(String planeinfo)
+        {
+            return new Plane.Plane();
+        }
+    }
+}
---
+++ AirTrafficMonitor/DataFormatter/DataFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plane
+
+namespace Calculator
+{
+    class DataFormatter
+    {
+        public DataFormatter(){}
+
+        public FormatString(Plane plane,)
+    }
+}
